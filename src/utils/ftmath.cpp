701034f2666df7f35b0c965632ec4302e6403240
Wilbert Lee <wilbert-lee@outlook.com>
2019-12-17 17:46:19 +0800

    Add files via upload

---
+++ src/utils/ftmath.cpp
@@ -0,0 +1,14 @@
+#include "ftmath.h"
+
+std::optional<long> parseLong(CString str) {
+	char *endptr;
+	const int radix = 10;
+
+	long out;
+	out = strtol(CT2A(str), &endptr, radix);
+	if (*endptr == '\0') {
+		return out;
+	}
+	return {};
+}
+
